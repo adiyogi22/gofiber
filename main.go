f6ab8290116e7018f0acbe0b912bb5125449a5ec
Pradeep Sapkota <lunalabs22@gmail.com>
2021-09-18 16:55:47 +0545

    project commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+)
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, Worldd!")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", helloWorld)
+
+	app.Get("/api/v1/jobs", jobs.GetJobs)
+	app.Get("/api/v1/jobs/:id", jobs.GetJob)
+	app.Post("/api/v1/jobs", jobs.NewJob)
+	app.Delete("/api/v1/jobs/:id", jobs.DeleteJob)
+}
+
+func main() {
+	app := fiber.New()
+
+	setupRoutes(app)
+	app.Listen(3000)
+}
